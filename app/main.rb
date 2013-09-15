50d3184b1973b34fdfa59650ca54ae94de5b3330
kmazanec <k.mazanec@gmail.com>
2013-09-15 16:55:15 -0500

    updated to latest versions

--- app/main.rb
+++ app/main.rb
@@ -1,10 +1,21 @@
 require_relative "../console.rb"
 
 print "Enter a state (AB) to view congressional delegation: "
-input = gets.chomp
-
+input = gets.chomp.upcase
 
 state_id = State.find_by(name: input).id
-Politician.where(state_id: state_id).each do |pol|
-  puts "#{Title.find(pol.title_id).name} #{pol.firstname} #{pol.lastname} (#{Party.find(pol.party_id).name})\n"
+sen_title_id = Title.find_by(name: "Sen").id
+rep_title_id = Title.find_by(name: "Rep").id
+puts "Senators from #{input}:"
+Politician.where(state_id: state_id, title_id: sen_title_id).order(:lastname).each do |pol|
+  puts "  #{pol.firstname} #{pol.lastname} (#{Party.find(pol.party_id).name})"
 end
+puts "Representatives from #{input}:"
+Politician.where(state_id: state_id, title_id: rep_title_id).order(:lastname).each do |pol|
+  puts "  #{pol.firstname} #{pol.lastname} (#{Party.find(pol.party_id).name})"
+end
+
+print "Enter a gender to analyze (M or F): "
+input = gets.chomp.upcase
+
+Politician.where(gender: input).count
